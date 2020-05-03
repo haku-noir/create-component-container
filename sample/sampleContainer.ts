c5099294b3dbce81932d00ceb113c0e8cc9e0087
haku-noir <hakunoir.00@gmail.com>
2020-05-03 23:00:10 +0900

    clean: 名称からconnectedを削除

--- sample/sampleContainer.ts
+++ sample/sampleContainer.ts
@@ -1,10 +1,10 @@
 import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import { RootState } from 'store';
-import { XXX, XXXStateAsProps, XXXDispatchAsProps } from 'components/XXX';
+import { XXX as XXXComp, XXXStateAsProps, XXXDispatchAsProps } from 'components/XXX';
 
 const mapStateToProps = (rootState: RootState): XXXStateAsProps => ({});
 
 const mapDispatchToProps = (dispatch: Dispatch): XXXDispatchAsProps => ({});
 
-export const connectedXXX = connect(mapStateToProps, mapDispatchToProps)(XXX);
+export const XXX = connect(mapStateToProps, mapDispatchToProps)(XXXComp);
