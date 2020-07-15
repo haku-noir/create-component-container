85129f9fb3b951b726dd70e8136de164c216d2ff
haku-noir <hakunoir.00@gmail.com>
2020-07-15 20:01:35 +0900

    change: containerの名前を変更

--- hooks/hooksContainer.tsx
+++ hooks/hooksContainer.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
 import { Action } from 'redux';
 import { useSelector, useDispatch } from 'react-redux';
-import { XXX } from 'components/XXX';
+import { XXX as XXXComp } from 'components/XXX';
 import { RootState } from 'store';
 
 type IProps = {};
 
-export const XXXCTR: React.FC<IProps> = (props: IProps) => {
+export const XXX: React.FC<IProps> = (props: IProps) => {
   const dispatch = useDispatch<Action>();
 
   const state = useSelector<RootState, RootState>(state => state);
 
   const _props = {}
 
-  return <XXX {..._props}/>;
+  return <XXXComp {..._props}/>;
 };
