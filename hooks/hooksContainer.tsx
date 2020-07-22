e725eb4774537d7768b2e02c205b45693bed34fa
haku-noir <hakunoir.00@gmail.com>
2020-07-23 03:55:48 +0900

    fix: dispatchの型を修正

--- hooks/hooksContainer.tsx
+++ hooks/hooksContainer.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Action } from 'redux';
+import { Dispatch, Action } from 'redux';
 import { useSelector, useDispatch } from 'react-redux';
 import { XXX as XXXComp } from 'components/XXX';
 import { RootState } from 'store';
@@ -7,7 +7,7 @@ import { RootState } from 'store';
 type IProps = {};
 
 export const XXX: React.FC<IProps> = (props: IProps) => {
-  const dispatch = useDispatch<Action>();
+  const dispatch = useDispatch<Dispatch<Action>>();
 
   const state = useSelector<RootState, RootState>(state => state);
 
